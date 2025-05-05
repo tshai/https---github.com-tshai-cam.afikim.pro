30dd9bff2167505e8a83c37ddd95bc167bb93ca3
zahi raifer <raiifert@gmail.com>
2025-05-05 12:40:12 +0300

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -2,9 +2,6 @@
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
 define('WP_DEBUG_DISPLAY', false);
-define('DB_NAME', 'mifgashim');
-define('DB_USER', 'wp_cam.afikim.pro');
-define('DB_PASSWORD', 'pqOewg');
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
